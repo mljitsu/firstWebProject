614bbb020fffe4012140575c19f2a322f341cc44
mljitsu <44201285+mljitsu@users.noreply.github.com>
2020-07-20 21:51:00 +0200

    Scripts and js
    Scripts for old browsers added and js file created

---
+++ resources/js/script.js
@@ -0,0 +1,11 @@
+/* eslint-env jquery */
+/* global document */
+
+$(document).ready(function () {
+ 
+    $('h1').click(function (){
+        $(this).css('background-color', '#ff0000')
+    })
+     
+
+});
