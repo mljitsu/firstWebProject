81823eadcaec1ec8cfa5ce471824f6c1d8366f3e
mljitsu <44201285+mljitsu@users.noreply.github.com>
2020-07-21 15:44:53 +0200

    Waypoint activated sticky class

--- resources/js/script.js
+++ resources/js/script.js
@@ -2,10 +2,15 @@
 /* global document */
 
 $(document).ready(function () {
- 
-    $('h1').click(function (){
-        $(this).css('background-color', '#ff0000')
-    })
-     
 
+    $('.js--section-features').waypoint(function(direction) {  /* Select js section feature as waypoint call function direction */
+        if (direction == "down") {
+            $('nav').addClass('sticky');                       /* Select nav, add class sticky if direction is down  */
+        } else {
+            $('nav').removeClass('sticky');                    /* selevt nav, remove class when direction is not down  */
+        }
+    }, {
+        offset: '65px;'                                         /* Offest so nav appears 65px before js--section-features  */
+    });
+    
 });
