8a49214d0b482ab479731454ac9d627edbffe966
mljitsu <44201285+mljitsu@users.noreply.github.com>
2020-07-23 12:46:14 +0200

    Main paige buttons scroll

--- resources/js/script.js
+++ resources/js/script.js
@@ -3,6 +3,8 @@
 
 $(document).ready(function () {
 
+    /* Sticky navigation */
+    
     $('.js--section-features').waypoint(function(direction) {  /* Select js section feature as waypoint call function direction */
         if (direction == "down") {
             $('nav').addClass('sticky');                       /* Select nav, add class sticky if direction is down  */
@@ -12,5 +14,19 @@ $(document).ready(function () {
     }, {
         offset: '65px;'                                         /* Offest so nav appears 65px before js--section-features  */
     });
+    
+    /* Main paige buttons scroll */ 
+    
+    $('.js--scroll-to-plan').click(function (){                                            /* Select jjs--scroll-to-plan on click has function */
+        $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);  /* Select html, body animate scroll to top of .js--section-plans with offset on top of 1s   */
+    });
+    
+    $('.js--scroll-to-start').click(function (){                                           
+        $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);  
+    });
+    
+    /* Main page nav scroll */
+    
+    
     
 });
