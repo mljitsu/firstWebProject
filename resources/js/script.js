a7d195cfd1bc5569da4665e70b96cfe9847ed09e
mljitsu <44201285+mljitsu@users.noreply.github.com>
2020-07-24 11:43:57 +0200

    Mobile nav

--- resources/js/script.js
+++ resources/js/script.js
@@ -45,28 +45,51 @@ $(document).ready(function () {
     
     /* Animations on scroll */ 
     
-   $('.js--wp-1').waypoint(function(direction) {
+   $('.js--wp-1').waypoint(function() {
        $('.js--wp-1').addClass('animate__fadeIn');
    }, {
        offset: '50%'
    }); 
     
-    $('.js--wp-2').waypoint(function(direction) {
+    $('.js--wp-2').waypoint(function() {
        $('.js--wp-2').addClass('animate__fadeInUp');
    }, {
        offset: '50%'
    }); 
     
-    $('.js--wp-3').waypoint(function(direction) {
+    $('.js--wp-3').waypoint(function() {
        $('.js--wp-3').addClass('animate__fadeIn');
    }, {
        offset: '50%'
    }); 
     
-    $('.js--wp-4').waypoint(function(direction) {
+    $('.js--wp-4').waypoint(function() {
        $('.js--wp-4').addClass('animate__pulse');
    }, {
        offset: '50%'
    }); 
+    
+
+    /* Mobile nav */ 
+    
+    $('.js--nav-icon').click(function(){
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon i');
+        
+        nav.slideToggle(200);
+        
+        if (icon.hasClass('ion-navicon-round')) {
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon-round');
+        } else {
+            icon.addClass('ion-navicon-round');
+            icon.removeClass('ion-close-round');
+        }        
+    });
+    
+    
+    
+    
+    
     
 });
